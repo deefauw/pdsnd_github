dd46a4b61a4fadf134711e64dccc484d34c00c67
Brad DeFauw <badefauw@gmail.com>
2019-01-31 18:11:44 -0600

    Updated string formatting in most popular month

--- bikeshare.py
+++ bikeshare.py
@@ -86,7 +86,7 @@ def time_stats(df):
 
     # TO DO: display the most common month
     popular_month = df['month'].mode()[0]
-    print('Most popular month: ', popular_month)
+    print('The most popular month is {}'.format(popular_month))
 
 
     # TO DO: display the most common day of week
